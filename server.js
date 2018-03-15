af2064392b5a30c163ddec5130c5a2bfc5cc8ffa
Ryan Butler <b6utle5r@gmail.com>
2018-03-15 17:25:24 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+
+require('dotenv').config()
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || 8000
+app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
